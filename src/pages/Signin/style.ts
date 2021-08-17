cebab36b11a977ab4b7ee5ed7333eaf0d4c0e115
José Augusto <jose.pasquali@snowmanlabs.com>
2021-08-16 22:08:08 -0300

    Firts commit

--- src/pages/Signin/style.ts
+++ src/pages/Signin/style.ts
@@ -1,5 +1,105 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
+import signinImgBackground from '../../assets/sign-in-background.png';
 
-export const Container = styled.div``;
-export const Content = styled.div``;
-export const Background = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  // faz com que todos os componentes de dentro tenham
+  // 100vh
+  align-items: stretch;
+`;
+export const Content = styled.div`
+  display: flex;
+  // aqui faz flex por culunas
+  flex-direction: column;
+  align-items: center;
+  // aqui centraliza x e y
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      color: #f4ede8;
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+      height: 56px;
+      border-radius: 10px;
+      border: 0;
+      // da padding so nas laterais
+      padding: 0 16px;
+      color: #312e38;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  // pega apenas os a depois do form
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
+`;
+export const Background = styled.div`
+  flex: 1;
+
+  background: url(${signinImgBackground}) no-repeat center;
+  // ajusta 100% do tamanho disponivel
+  background-size: cover;
+`;
