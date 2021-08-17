940c070ea50c02a6b748c1ec2b1c3efd35ce3199
José Augusto <jose.pasquali@snowmanlabs.com>
2021-08-16 21:45:20 -0300

    Firts commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+import Signin from './pages/Signin/index';
+
+const App: React.FC = () => (
+  <>
+    <Signin />
+    <GlobalStyle />
+  </>
+);
+export default App;
