9eaa0b21538f9848019069a7ce37caba28c63d72
José Augusto <jose.pasquali@snowmanlabs.com>
2021-08-18 11:49:42 -0300

    Unform

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 import { Container } from './style';
 
 // InputHTMLAttributes: interface responsavel por receber todos os atributos
@@ -16,13 +17,41 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 // quando recebe a props e passa com spred {...props}
 // quer dizer que vai atribuir tudo oque passar para o input
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {/* aqui utiliza Icon e nao icon para que o react intenda que é um componente
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  // a useRef server para disponibilizar o componente para busca no dom
+  const inputRef = useRef(null);
+
+  // o useField serve para registrar o componente na dom
+  /*
+   * fieldName -> nome do fild
+   * defaultValue -> valor inicial do input
+   * error -> caso queira tratar erros
+   * registerField -> server para registrar o componente no unform
+   */
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    // registra o fild na dom
+    registerField({
+      // aqui é o nome do componente
+      name: fieldName,
+      // ref é a referencia
+      ref: inputRef.current,
+      // quando for pegar o valor do componente de qual tag ele buscara
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container>
+      {/* aqui utiliza Icon e nao icon para que o react intenda que é um componente
     na verdade é mais um rename  */}
-    {Icon && <Icon size={20} />}
-    <input {...rest} />
-  </Container>
-);
+      {Icon && <Icon size={20} />}
+      {/* aqui o ref={inputRef} serve para registra a ref do componente */}
+      {/* o defaultValue serve para quando quer deixar valor setado exp: edicao */}
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} />
+    </Container>
+  );
+};
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form } from '@unform/web';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { Container, Content, Background } from './style';
 
@@ -7,31 +8,41 @@ import Button from '../../components/Button/index';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
 
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form>
-        <h1>Faça seu cadastro</h1>
+  return (
+    <Container>
+      <Background />
 
-        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
-        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit"> Cadastrar</Button>
-      </form>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        {/*
+            aqui para passar para o form qual valor inicial do mesmo
+            <Form initialData={{ name: 'José' }} onSubmit={handleSubmit}>
+          */}
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
 
-      <a href="new">
-        <FiArrowLeft /> Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit"> Cadastrar</Button>
+        </Form>
+
+        <a href="new">
+          <FiArrowLeft /> Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
